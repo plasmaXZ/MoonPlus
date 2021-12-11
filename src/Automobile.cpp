5c895ac64f097c1d39fc7a850d02af5890961cd1
vitor <vitor@bitbyte.com.br>
2021-12-11 15:45:01 -0300

    Merge Vehicle and Automobile classes
    Having those 2 classes was redundant.

--- src/Automobile.cpp
+++ src/Automobile.cpp
@@ -23,12 +23,152 @@
 // SOFTWARE.
 
 #include "Automobile.hpp"
+#include "Transmission.cpp"
 
 namespace automobile
 {
-	Automobile::Automobile(CAutomobile* vehicle) : Vehicle(vehicle)
+	Automobile::Automobile(CAutomobile* vehicle) : Physical(vehicle)
 	{
 		m_Vehicle = vehicle;
+	}
+
+	uint32_t Automobile::GetHandlingAddress()
+	{
+		return reinterpret_cast<uintptr_t>(m_Vehicle->m_pHandlingData);
+	}
+
+	uint32_t Automobile::GetFlyingHandlingAddress()
+	{
+		return reinterpret_cast<uintptr_t>(m_Vehicle->m_pFlyingHandlingData);
+	}
+
+	uint32_t Automobile::GetHandlingFlags()
+	{
+		return m_Vehicle->m_nHandlingFlagsIntValue;
+	}
+
+	void Automobile::SetHandlingFlags(uint32_t flags)
+	{
+		m_Vehicle->m_nHandlingFlagsIntValue = static_cast<eVehicleHandlingFlags>(flags);
+	}
+
+	float Automobile::GetWheelScale()
+	{
+		return m_Vehicle->m_fWheelScale;
+	}
+
+	void Automobile::SetWheelScale(float scale)
+	{
+		m_Vehicle->m_fWheelScale = scale;
+	}
+
+	float Automobile::GetSteerAngle()
+	{
+		return m_Vehicle->m_fSteerAngle;
+	}
+
+	void Automobile::SetSteerAngle(float angle)
+	{
+		m_Vehicle->m_fSteerAngle = angle;
+	}
+
+	float Automobile::GetGasPedal()
+	{
+		return m_Vehicle->m_fGasPedal;
+	}
+
+	void Automobile::SetGasPedal(float gas)
+	{
+		m_Vehicle->m_fGasPedal = gas;
+	}
+
+	float Automobile::GetBrakePedal()
+	{
+		return m_Vehicle->m_fBreakPedal;
+	}
+
+	void Automobile::SetBrakePedal(float brake)
+	{
+		m_Vehicle->m_fBreakPedal = brake;
+	}
+
+	uint8_t Automobile::GetCreatedBy()
+	{
+		return m_Vehicle->m_nCreatedBy;
+	}
+
+	void Automobile::SetCreatedBy(uint8_t createdBy)
+	{
+		m_Vehicle->m_nCreatedBy = createdBy;
+	}
+
+	uint16_t Automobile::GetRouteSeed()
+	{
+		return m_Vehicle->m_nForcedRandomRouteSeed;
+	}
+
+	void Automobile::SetRouteSeed(uint16_t seed)
+	{
+		m_Vehicle->m_nForcedRandomRouteSeed = seed;
+	}
+
+	float Automobile::GetDirtLevel()
+	{
+		return m_Vehicle->m_fDirtLevel;
+	}
+
+	void Automobile::SetDirtLevel(float level)
+	{
+		m_Vehicle->m_fDirtLevel = level;
+	}
+
+	float Automobile::GetGearChangeCount()
+	{
+		return m_Vehicle->m_fGearChangeCount;
+	}
+
+	void Automobile::SetGearChangeCount(float count)
+	{
+		m_Vehicle->m_fGearChangeCount = count;
+	}
+
+	float Automobile::GetWheelSpinForAudio()
+	{
+		return m_Vehicle->m_fWheelSpinForAudio;
+	}
+
+	void Automobile::SetWheelSpinForAudio(float wheelSpin)
+	{
+		m_Vehicle->m_fWheelSpinForAudio = wheelSpin;
+	}
+
+	void Automobile::BlowUp(uintptr_t damager, uint8_t hideExplosion)
+	{
+		if(!m_Vehicle)
+			return;
+
+		CEntity *entity = reinterpret_cast<CEntity*>(damager);
+		m_Vehicle->BlowUpCar(entity, hideExplosion);
+	}
+
+	uint32_t Automobile::GetContactWheelsCount()
+	{
+		return m_Vehicle->GetNumContactWheels();
+	}
+
+	float Automobile::GetWheelWidth(bool fromRear)
+	{
+		return m_Vehicle->FindWheelWidth(fromRear);
+	}
+
+	transmission::Transmission Automobile::GetTransmission()
+	{
+		return transmission::Transmission(&m_Vehicle->m_pHandlingData->m_transmissionData);
+	}
+
+	vehicle_audio::VehicleAudio Automobile::GetAudio()
+	{
+		return vehicle_audio::VehicleAudio(&m_Vehicle->m_vehicleAudio);
 	}
 
 	float Automobile::GetWheelSuspensionCompression(uint8_t wheel)
@@ -105,16 +245,98 @@ namespace automobile
 		m_Vehicle->PlaceOnRoadProperly();
 	}
 
+	bool IsVehiclePointerValid(uintptr_t address)
+	{
+		return IsVehiclePointerValid(reinterpret_cast<CVehicle*>(address));
+	}
+
 	Automobile FromMemory(uintptr_t address)
 	{
 		return Automobile(reinterpret_cast<CAutomobile*>(address));
 	}
 
+	float GetAirResistanceMult()
+	{
+		return CVehicle::m_fAirResistanceMult;
+	}
+
+	void SetAirResistanceMult(float mult)
+	{
+		CVehicle::m_fAirResistanceMult = mult;
+	}
+
+	void SetWheelSpinLoss(float loss)
+	{
+		WS_ALREADY_SPINNING_LOSS = loss;
+	}
+
+	float GetWheelSpinLoss()
+	{
+		return WS_ALREADY_SPINNING_LOSS;
+	}
+
+	void SetWheelSpinValues(float targetRate, float inAirTargetRate, float riseRate,
+		float fallRate)
+	{
+		CVehicle::WHEELSPIN_TARGET_RATE = targetRate;
+		CVehicle::WHEELSPIN_INAIR_TARGET_RATE = inAirTargetRate;
+		CVehicle::WHEELSPIN_RISE_RATE = riseRate;
+		CVehicle::WHEELSPIN_FALL_RATE = fallRate;
+	}
+
+	void ResetWheelSpinValues()
+	{
+		CVehicle::WHEELSPIN_TARGET_RATE = 1.0f;
+		CVehicle::WHEELSPIN_INAIR_TARGET_RATE = 10.0f;
+		CVehicle::WHEELSPIN_RISE_RATE = 0.95f;
+		CVehicle::WHEELSPIN_FALL_RATE = 0.7f;
+	}
+
+	float GetBikeWheelTurnForce()
+	{
+		return fTweakBikeWheelTurnForce;
+	}
+
+	void SetBikeWheelTurnForce(float force)
+	{
+		fTweakBikeWheelTurnForce = force;
+	}
+
 	void initialize(sol::table& module)
 	{
-		module.new_usertype<Automobile>("automobile",
-			sol::base_classes, sol::bases<Placeable, Entity, Physical, Vehicle>(),
+		module.set_function("is_vehicle_pointer_valid", &IsVehiclePointerValid);
+		module.set_function("set_wheel_spin_values", &SetWheelSpinValues);
+		module.set_function("reset_wheel_spin_values", &ResetWheelSpinValues);
+		module.set_function("get_wheel_spin_loss", &GetWheelSpinLoss);
+		module.set_function("set_wheel_spin_loss", &SetWheelSpinLoss);
+		module.set_function("get_air_resistance", &GetAirResistanceMult);
+		module.set_function("set_air_resistance", &SetAirResistanceMult);
+		module.set_function("get_bike_wheel_turn_force", &GetBikeWheelTurnForce);
+		module.set_function("set_bike_wheel_turn_force", &SetBikeWheelTurnForce);
+
+		module.new_usertype<Automobile>("vehicle",
+			sol::base_classes, sol::bases<Placeable, Entity, Physical>(),
 			"from_memory", sol::factories(&FromMemory),
+			"get_handling_pointer", &Automobile::GetHandlingAddress,
+			"get_flying_handling_pointer", &Automobile::GetFlyingHandlingAddress,
+			"handling_flags", sol::property(&Automobile::GetHandlingFlags,
+					&Automobile::SetHandlingFlags),
+			"wheel_scale", sol::property(&Automobile::GetWheelScale, &Automobile::SetWheelScale),
+			"steer_angle", sol::property(&Automobile::GetSteerAngle, &Automobile::SetSteerAngle),
+			"gas_pedal", sol::property(&Automobile::GetGasPedal, &Automobile::SetGasPedal),
+			"brake_pedal", sol::property(&Automobile::GetBrakePedal, &Automobile::SetBrakePedal),
+			"created_by", sol::property(&Automobile::GetCreatedBy, &Automobile::SetCreatedBy),
+			"route_seed", sol::property(&Automobile::GetRouteSeed, &Automobile::SetRouteSeed),
+			"dirt_level", sol::property(&Automobile::GetDirtLevel, &Automobile::SetDirtLevel),
+			"gear_change_count", sol::property(&Automobile::GetGearChangeCount,
+					&Automobile::SetGearChangeCount),
+			"wheel_spin_audio", sol::property(&Automobile::GetWheelSpinForAudio,
+					&Automobile::SetWheelSpinForAudio),
+			"blow_up", &Automobile::BlowUp,
+			"get_contact_wheels_count", &Automobile::GetContactWheelsCount,
+			"get_wheel_width", &Automobile::GetWheelWidth,
+			"get_transmission", &Automobile::GetTransmission,
+			"get_audio", &Automobile::GetAudio,
 			"get_wheel_suspension_compression", &Automobile::GetWheelSuspensionCompression,
 			"get_wheel_rotation", &Automobile::GetWheelRotation,
 			"get_wheel_position", &Automobile::GetWheelPosition,
@@ -127,5 +349,23 @@ namespace automobile
 			"nitro_value", sol::property(&Automobile::GetNitroValue, &Automobile::SetNitroValue),
 			"tell_to_go_to", &Automobile::TellToGoTo,
 			"place_on_road_properly", &Automobile::PlaceOnRoadProperly);
+
+		module.new_enum<true>("vehicle_created_by",
+			"RANDOM_VEHICLE", 1,
+			"MISSION_VEHICLE", 2,
+			"PARKED_VEHICLE", 3,
+			"PERMANENT_VEHICLE", 4);
+
+		module.new_enum<true>("wheel_enum",
+			"FRONT_LEFT", 0,
+			"REAR_LEFT", 1,
+			"FRONT_RIGHT", 2,
+			"REAR_RIGHT", 3);
+
+		module.new_enum<true>("wheel_state",
+			"NORMAL", 0,
+			"SPINNING", 1,
+			"SKIDDING", 2,
+			"FIXED", 3);
 	}
 }
--- src/Automobile.hpp
+++ src/Automobile.hpp
@@ -24,22 +24,52 @@
 
 #pragma once
 
+#include "Transmission.hpp"
+#include "VehicleAudio.hpp"
 #include "CAutomobile.h"
-#include "Vehicle.hpp"
+#include "Physical.hpp"
 
 namespace automobile
 {
 	using Placeable = placeable::Placeable;
 	using Entity = entity::Entity;
 	using Physical = physical::Physical;
-	using Vehicle = vehicle::Vehicle;
+	using Transmission = transmission::Transmission;
+	using VehicleAudio = vehicle_audio::VehicleAudio;
 
-	class Automobile : public Vehicle
+	class Automobile : public Physical
 	{
 	private:
 		CAutomobile* m_Vehicle;
 	public:
 		Automobile(CAutomobile* vehicle);
+		uintptr_t GetHandlingAddress();
+		uintptr_t GetFlyingHandlingAddress();
+		uint32_t GetHandlingFlags();
+		void SetHandlingFlags(uint32_t flags);
+		float GetWheelScale();
+		void SetWheelScale(float scale);
+		float GetSteerAngle();
+		void SetSteerAngle(float angle);
+		float GetGasPedal();
+		void SetGasPedal(float gas);
+		float GetBrakePedal();
+		void SetBrakePedal(float brake);
+		uint8_t GetCreatedBy();
+		void SetCreatedBy(uint8_t createdBy);
+		uint16_t GetRouteSeed();
+		void SetRouteSeed(uint16_t seed);
+		float GetDirtLevel();
+		void SetDirtLevel(float level);
+		float GetGearChangeCount();
+		void SetGearChangeCount(float count);
+		float GetWheelSpinForAudio();
+		void SetWheelSpinForAudio(float wheelSpin);
+		void BlowUp(uintptr_t damager, uint8_t hideExplosion);
+		uint32_t GetContactWheelsCount();
+		float GetWheelWidth(bool fromRear);
+		Transmission GetTransmission();
+		VehicleAudio GetAudio();
 		float GetWheelSuspensionCompression(uint8_t wheel);
 		float GetWheelRotation(uint8_t wheel);
 		float GetWheelPosition(uint8_t wheel);
@@ -56,6 +86,16 @@ namespace automobile
 		void PlaceOnRoadProperly();
 	};
 
+	bool IsVehiclePointerValid(uintptr_t address);
 	Automobile FromMemory(uintptr_t address);
+	float GetAirResistanceMult();
+	void SetAirResistanceMult(float mult);
+	float GetWheelSpinLoss();
+	void SetWheelSpinLoss(float loss);
+	void SetWheelSpinValues(float targetRate, float inAirTargetRate, float riseRate,
+		float fallRate);
+	void ResetWheelSpinValues();
+	float GetBikeWheelTurnForce();
+	void SetBikeWheelTurnForce(float force);
 	void initialize(sol::table& module);
 }
